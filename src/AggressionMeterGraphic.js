e4751d7a9ceed6fb81a9543d5727bdfdf226c5f3
Ryan Badour <rybadour@gmail.com>
2012-01-22 17:14:03 -0500

    Revert "Merged in Stephens Branch with the Aggression Meter graphic"
    This reverts commit c9eefdc25d32711cbcfbc61b3468f3c6786927b8.
    
    Removing the aggression meter code, we don't need it anymore.

--- src/AggressionMeterGraphic.js
+++ src/AggressionMeterGraphic.js
@@ -6,6 +6,7 @@
 */
 function AggressionMeterGraphic(x, y, height, length, ctx, meter)
 {
+	this.barSize = 0;
 	this.ctx = ctx;
 	this.x = x;
 	this.y = y;
@@ -19,18 +20,14 @@ function AggressionMeterGraphic(x, y, height, length, ctx, meter)
 AggressionMeterGraphic.prototype.draw = function()
 { 
 	var currentSize = this.monitoredObject.read() - 50;
-	var barSize = currentSize * this.increment;
-
-	this.ctx.save();
-	this.ctx.translate(this.x, this.y); 
-
-  // Draw a border around the meter
-	this.ctx.strokeStyle = "rgb(200, 200, 200)";
-	this.ctx.strokeRect(0, 0, this.length, this.height);
-
-  // Create a linear gradient from the center to the edge
 	var endColour;
 	var endPoint;
+	
+	this.barSize = currentSize * this.increment;
+	this.ctx.save();
+	this.ctx.translate(this.x, this.y); 
+	this.ctx.strokeStyle = "rgb(200, 200, 200)";
+	this.ctx.strokeRect(0, 0, this.length, this.height);
 	if (this.monitoredObject.read() >= 50)
 	{
 		endColour = 'red';
@@ -41,11 +38,10 @@ AggressionMeterGraphic.prototype.draw = function()
 		endColour = 'blue';
 		endPoint = 0;
 	}
-	var linearGrad = ctx.createLinearGradient(this.center, 0, endPoint, 0);
-	linearGrad.addColorStop(0, 'rgb(100, 100, 100)');
-	linearGrad.addColorStop(1, endColour);
-	this.ctx.fillStyle = linearGrad;
-	this.ctx.fillRect(this.center, 1, barSize, this.height-2);
-
+	var lingrad = ctx.createLinearGradient(this.center, 0, endPoint, 0);
+	lingrad.addColorStop(0, 'rgb(100, 100, 100)');
+	lingrad.addColorStop(1, endColour);
+	this.ctx.fillStyle = lingrad;
+	this.ctx.fillRect(this.center, 1, this.barSize, this.height-2);
 	this.ctx.restore();
 }
