9c61540694cd01235226f98b8f450f1d7a80788c
Ryan Badour <rybadour@gmail.com>
2012-02-03 17:04:37 -0500

    Fixed: The Angry invader now uses a movement pattern like the yellow invader and no longer crashes the game when it spawns.
    Signed-off-by: Ryan Badour <rybadour@gmail.com>

--- src/AngryInvader.js
+++ src/AngryInvader.js
@@ -1,5 +1,40 @@
-function AngryInvader(game, x, y) {
-  Enemy.call(this, game, x, y, CONST.ANGRY_INVADER_IMAGE, CONST.ANGRY_INVADER_HEALTH);
+function AngryInvader(game, x, y)
+{
+  // Size of the wave of its path
+  var width = 500;
+  var height = 800;
+  var speed = 8;
+
+  var a = new Bezier(speed,
+    [
+      {x: x,            y: y},
+      {x: x - width/2,  y: y + height/2},
+      {x: x + width/2,  y: y + height/2},
+      {x: x,            y: y + height}
+    ]
+  );
+  var b = new Bezier(speed,
+    [
+      {x:0,         y: 0},
+      {x: -width/2, y: height/2},
+      {x: width/2,  y: height/2},
+      {x: 0,        y: height}
+    ]
+  );
+  var c = new Bezier(speed,
+    [
+      {x:0,         y: 0},
+      {x: -width/2, y: height/2},
+      {x: width/2,  y: height/2},
+      {x: 0,        y: height}
+    ]
+  );
+  var movements = [a, b, c];
+
+  Enemy.call(this, game, x, y, CONST.ANGRY_INVADER_IMAGE, CONST.ANGRY_INVADER_POINTS,
+    CONST.ANGRY_INVADER_HEALTH, CONST.ANGRY_INVADER_SPEED, CONST.ANGRY_INVADER_DAMAGE, CONST.ANGRY_INVADER_FIRE_RATE,
+    new MovementPattern(this, movements), null
+  );
   
   this.xVelo = 0;
   this.yVelo = CONST.ANGRY_INVADER_SPEED;
@@ -11,61 +46,3 @@ function AngryInvader(game, x, y) {
   this.amp = Math.random() * 50 + 50;
 }
 obj.extend(AngryInvader, Enemy);
-
-AngryInvader.prototype.destroy = function() 
-{
-  this.game.addEntity(new Explosion(this.game, this.x, this.y));
-}
-
-AngryInvader.prototype.collide = function() 
-{
-  var result = this.game.aabb.intersects(
-    new AabbTree.AxisAlignedBox(
-      [this.x - this.w/2, this.y - this.h/2],
-      [this.w, this.h]
-    )
-  );
-
-  for (id in result) 
-  {
-    var entity = this.game.entities[id];
-    if (!entity.removeFromWorld) 
-    {
-      if (entity instanceof PlayerShip) 
-      {
-        this.removeFromWorld = true;
-        entity.health -= CONST.ANGRY_INVADER_DAMAGE;
-        this.game.score += CONST.ANGRY_INVADER_POINTS;
-      }
-    }
-  }
-  AngryInvader.zuper.collide.call(this);
-}
-
-AngryInvader.prototype.update = function() 
-{
-  this.cx += this.xVelo * this.game.clockTick;
-  this.cy += this.yVelo * this.game.clockTick;
-  this.angle += 2 * this.game.clockTick;
-  this.x = this.cx + this.amp * Math.sin(this.angle);
-  this.y = this.cy;
-  
-  if (this.health <= 0) 
-  {
-    this.removeFromWorld = true;
-  }
-  if (this.offBottom()) 
-  {
-    this.removeFromWorld = true;
-  }
-
-  AngryInvader.zuper.update.call(this);
-}
-
-AngryInvader.prototype.draw = function(ctx) 
-{
-  this.drawHealthBar(ctx);
-  this.drawSpriteCentered(ctx);
-  AngryInvader.zuper.draw.call(this, ctx);
-}
-
