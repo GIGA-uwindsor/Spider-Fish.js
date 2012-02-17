74367054e169dacc2ef6085facea7cf751359106
imperishable <aaronmonminie@hotmail.com>
2012-02-17 15:39:21 -0500

    Added new ShotPattern's and a new tri-shooting invader type

--- src/BossInvader.js
+++ src/BossInvader.js
@@ -2,7 +2,7 @@ function BossInvader(game, x, y)
 {
   Enemy.call(this, game, x, y, CONST.BOSS_INVADER_IMAGE, CONST.BOSS_INVADER_POINTS,
     CONST.BOSS_INVADER_HEALTH, CONST.BOSS_INVADER_SPEED, CONST.BOSS_INVADER_DAMAGE, CONST.BOSS_INVADER_FIRE_RATE,
-    null, new SingleShotPattern(this)
+    null, new CurtainShotPattern(this)
   );
 }
 obj.extend(BossInvader, Enemy);
