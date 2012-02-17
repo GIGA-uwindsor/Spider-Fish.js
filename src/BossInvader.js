c331aa01b31ed0c83ce2f188ebafd003c2ac5c38
Ryan Badour <rybadour@gmail.com>
2012-02-17 16:22:09 -0500

    Updated: The Boss Invader now cycles between firing patterns every few seconds.
    Updated: Increased the health of the boss invader to be harder.
    
    Signed-off-by: Ryan Badour <rybadour@gmail.com>

--- src/BossInvader.js
+++ src/BossInvader.js
@@ -2,7 +2,35 @@ function BossInvader(game, x, y)
 {
   Enemy.call(this, game, x, y, CONST.BOSS_INVADER_IMAGE, CONST.BOSS_INVADER_POINTS,
     CONST.BOSS_INVADER_HEALTH, CONST.BOSS_INVADER_SPEED, CONST.BOSS_INVADER_DAMAGE, CONST.BOSS_INVADER_FIRE_RATE,
-    null, new SingleShotPattern(this)
+    null, null
   );
+
+  this.possiblePatterns = [
+    new SingleShotPattern(this),
+    new SingleShotPattern(this)
+  ];
+
+  this.alternateWeaponDelay = 2;  // secs
+  this.timeUntilAlternate = 0;
+
+  this.nextPattern = 0;
 }
 obj.extend(BossInvader, Enemy);
+
+BossInvader.prototype.update = function ()
+{
+  if (this.timeUntilAlternate <= 0)
+  {
+    this.firingPattern = this.possiblePatterns[this.nextPattern];
+
+    this.nextPattern++;
+    if (this.nextPattern >= this.possiblePatterns.length)
+      this.nextPattern = 0;
+
+    this.timeUntilAlternate = this.alternateWeaponDelay;
+  }
+
+  this.timeUntilAlternate -= this.game.clockTick;
+
+  BossInvader.zuper.update.call(this);
+}
